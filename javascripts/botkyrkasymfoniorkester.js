8d13d3220a0aae5f64d10e2b5b56a07f8d4c5590
Teitur Arnarson <teitur.arnarson@gmail.com>
2012-10-20 01:07:42 +0200

    added javascript links and footer

---
+++ javascripts/botkyrkasymfoniorkester.js
@@ -0,0 +1,35 @@
+$(document).ready(function()
+{
+  $("div.bs_main_frame").load("content/home.html");
+
+  $("a#main_menu_home").click(function()
+  {
+    $("div.bs_main_frame").load("content/home.html");
+  });
+
+  $("a#main_menu_about").click(function()
+  {
+    $("div.bs_main_frame").load("content/about.html");
+  });
+
+  $("a#main_menu_next_concert").click(function()
+  {
+    $("div.bs_main_frame").load("content/next_concert.html");
+  });
+
+  $("a#main_menu_concerts").click(function()
+  {
+    $("div.bs_main_frame").load("content/concerts.html");
+  });
+
+  $("a#main_menu_contact").click(function()
+  {
+    $("div.bs_main_frame").load("content/contact.html");
+  });
+
+  $("a#main_menu_members").click(function()
+  {
+    $("div.bs_main_frame").load("content/members.html");
+  });
+});
+
